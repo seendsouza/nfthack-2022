c1cf7b32235e96f7d1b312a574de4c9413f4f0ea
Victor Wang <victorwang2001@gmail.com>
2022-01-14 14:55:17 -0500

    create vault routes

--- server/main.py
+++ server/main.py
@@ -2,10 +2,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def test():
+@app.route("/start-lending")
+def startLending():
+    """
+    generate wallet, transfer axie to wallet, create axie account
+    """
     return "Hello World!"
 
+@app.route("/return-axies")
+def returnAxies():
+    """
+    return the axies to their original owner
+    """
+    return "Hello World!"
+
+@app.route("/list-lent-axies")
+def listListAxies():
+    """
+    returns a list of all the axies that are lent out
+    """
+    return "Hello World!"
+
+@app.route("/use-axie-account")
+def useAxieAccount():
+    """
+    get axie account login info
+    """
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
