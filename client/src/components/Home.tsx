c4beeefa31505ebf9ea3d80b5e2659c34ea38f68
Victor Wang <victorwang2001@gmail.com>
2022-01-15 17:49:15 -0500

    nice homepage

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,15 @@
-import Banner from "./Banner";
-
 function Home() {
-  return <div> </div>;
+  return <div>
+    <div className="bg-black w-full h-1/2 text-white h-fit">
+      <h1 className="w-full pt-80 text-9xl font-extrabold">
+        Scholar
+      </h1>
+      <h3 className="w-full pb-32 text-3xl font-extrabold">
+        A platform where Axies go to to find their new best friends.
+      </h3>
+    </div>
+
+  </div>;
 }
 
 export default Home;
