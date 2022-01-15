9023db0826d71c221acd4ab1b1013a8c130933d6
nazariyv <nazariy@inbox.ru>
2022-01-15 15:01:18 +0000

    adds: wallet creation

---
+++ server/README.md
@@ -0,0 +1,4 @@
+# Developer Guide
+
+1. Use python==3.8.12
+2. pip install -r requirements.txt
--- server/utils.py
+++ server/utils.py
@@ -1,3 +1,14 @@
+from dataclasses import dataclass
+from secrets import token_bytes
+from coincurve import PublicKey
+from sha3 import keccak_256
+
+@dataclass
+class Wallet(frozen=True):
+    private_key: bytes
+    public_key: bytes
+    addr: bytes
+
 def cleanseAxieWalletData(data):
     """
     remove sensitive like username and password
@@ -10,3 +21,18 @@ def cleanseAxieWalletData(data):
             "isCurrentlyUsed": axieWallet["isCurrentlyUsed"],
             "rentedAt": axieWallet["rentedAt"],
         }, data))
+
+# source: https://www.arthurkoziel.com/generating-ethereum-addresses-in-python/
+def createWallet() -> Wallet:
+    private_key = keccak_256(token_bytes(32)).digest()
+    public_key = PublicKey.from_valid_secret(private_key).format(compressed=False)[1:]
+    addr = keccak_256(public_key).digest()[-20:]
+
+    # print('private_key:', private_key.hex())
+    # print('eth addr: 0x' + addr.hex())
+
+    ### Output (Random each time) ###
+    # private_key: 7bf19806aa6d5b31d7b7ea9e833c202e51ff8ee6311df6a036f0261f216f09ef
+    # eth addr: 0x3db763bbbb1ac900eb2eb8b106218f85f9f64a13
+
+    return Wallet(private_key, public_key, addr)
