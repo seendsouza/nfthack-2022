a91ebe2a4d43793413dcf479083560929669a018
Victor Wang <victorwang2001@gmail.com>
2022-01-14 20:39:06 -0500

    fix env variable

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,20 +1,21 @@
 import type { Axie, LoginInfo } from "./types";
 
-const serverUri: string = "http://localhost:5000";
+const serverUri = import.meta.env.VITE_SERVER_URI;
 
 export const startLending = async (lenderWalletAddr: string): Promise<string> => {
   return fetch(`${serverUri}/start-lending/${lenderWalletAddr}`).then(res => res.json()).then(res => res.axieWalletAddr);
 };
 
 export const finishTransfer = async (lenderWalletAddr: string, axieWalletAddr: string): Promise<void> => {
-  return fetch(`${serverUri}/finish-transfer/${lenderWalletAddr}/${axieWalletAddr}`).then(res => void);
+  return fetch(`${serverUri}/finish-transfer/${lenderWalletAddr}/${axieWalletAddr}`).then(() => {return;});
 }
 
 export const returnAxies = async (axieWalletAddr: string): Promise<void> => {
-  return fetch(`${serverUri}/return-axies/${axieWalletAddr}`).then(res => void);
+  return fetch(`${serverUri}/return-axies/${axieWalletAddr}`).then(() => {return;});
 }
 
 export const getLentAxies = async (): Promise<Axie[]> => {
+  console.log(serverUri);
   return fetch(`${serverUri}/list-lent-axies`).then(res => res.json()).then(res => res.data);
 };
 
